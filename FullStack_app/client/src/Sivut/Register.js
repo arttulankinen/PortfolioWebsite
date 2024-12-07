f08ea5d5a8da25c5b9c3162ccf94984978595879
Arttu Lankinen <155956454+arttulankinen@users.noreply.github.com>
2024-12-07 18:16:38 +0200

    Update Register.js

--- FullStack_app/client/src/Sivut/Register.js
+++ FullStack_app/client/src/Sivut/Register.js
@@ -8,7 +8,7 @@ function Register() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const BaseURL = "https://artunportfolio.onrender.com";
+  const BaseURL = "https://artunportfolio.onrender.com/";
 
   const handleRegister = async (e) => {
     e.preventDefault();
