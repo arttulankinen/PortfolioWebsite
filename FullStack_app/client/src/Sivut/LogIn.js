e66cffd711481d1484a566722db1a5e99c2297c7
Arttu Lankinen <155956454+arttulankinen@users.noreply.github.com>
2024-12-07 18:24:52 +0200

    Update LogIn.js

--- FullStack_app/client/src/Sivut/LogIn.js
+++ FullStack_app/client/src/Sivut/LogIn.js
@@ -7,7 +7,7 @@ function LogIn() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const BaseURL = "https://artunportfolio.onrender.com/";
+  const BaseURL = "https://artunportfolio.onrender.com";
   
   const handleLogin = async (e) => {
     e.preventDefault();
